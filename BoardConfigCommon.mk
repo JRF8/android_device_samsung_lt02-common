4ef84755745e27af0612861660a837b6d4f9e066
jrf8 <jesse.frazier.8@gmail.com>
2022-02-25 13:04:22 -0500

    remove TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi- line

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,6 @@ BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Kernel Source
 TARGET_KERNEL_CONFIG := pxa986_lt02_defconfig
