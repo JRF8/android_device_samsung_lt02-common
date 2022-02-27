037eec774e7288b4194b136874adb84d4ca54245
jrf8 <jesse.frazier.8@gmail.com>
2022-02-26 22:38:53 -0500

    changing TARGET_KERNEL_CONFIG to reference asasoft

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -49,7 +49,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 
 # Kernel Source
-TARGET_KERNEL_CONFIG := pxa986_lt02_defconfig
+TARGET_KERNEL_CONFIG := asasoft_pxa986_lt02_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/lt02
 
 # Partitions
